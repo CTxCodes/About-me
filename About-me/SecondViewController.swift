34293807a7a8332b83eb9da0311dc098ab39e46c
Corey Townsend <CoreyTownsend12@gmail.com>
2019-03-25 16:11:46 -0400

    Initial Commit

---
+++ About-me/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  About-me
+//
+//  Created by Corey Townsend on 10/4/18.
+//  Copyright © 2018 coto.inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
